4a5d5e8a1b5b75626f79cadf5004232a4afb89d9
Shubham Yadav <sy3489010@gmail.com>
2026-05-07 12:31:29 +0530

    socket error fixed

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8080", {
+export const socket = io("https://instantvote-backend.onrender.com", {
   transports: ["websocket"]
 });
